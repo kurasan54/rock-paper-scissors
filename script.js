fe0833c1291fb80b29c9cec694c26ddf220cc1bf
Muazzam Amran <41406014+kurasan54@users.noreply.github.com>
2026-06-07 17:59:53 +0800

    Added getHumanChoice function and added IDs to help determine rock-paper-scissors algorithm

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let computerChoice = "";
+let computerChoiceID;
 
 function getComputerChoice(){
         let choice = Math.random() * 3;
@@ -7,17 +11,36 @@ function getComputerChoice(){
     switch (choice){
         case 0:
             computerChoice = "rock";
+            computerChoiceID = 0;
             break;
         case 1:
             computerChoice = "paper";
+            computerChoiceID = 1;
             break;
         case 2:
             computerChoice = "scissors";
+            computerChoiceID = 2;
             break;
     }
 }
 
 let playerChoice = "";
+let playerChoiceID;
 function getHumanChoice(){
     playerChoice = prompt("Rock, Paper or Scissors?")
+    playerChoice = playerChoice.toLowerCase();
+
+    switch (playerChoice){
+        case "rock":
+            playerChoiceID = 0;
+            break;
+        case "paper":
+            playerChoiceID = 1;
+            break;
+        case "scissors":
+            playerChoiceID = 2;
+            break;
+    }
 }
+
+getHumanChoice();
