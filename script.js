8833d2bacabea947cb2bd19c6d389a1579fccfa6
Muazzam Amran <41406014+kurasan54@users.noreply.github.com>
2026-06-07 18:39:44 +0800

    Added function playRound and displayWinner to play a game of RPS and display its winner

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 
 let computerChoice = "";
@@ -43,4 +43,58 @@ function getHumanChoice(){
     }
 }
 
-getHumanChoice();
+let whoWonMessage = "";
+let messageID = -1;
+function playRound(){
+    getHumanChoice();
+    getComputerChoice();
+
+    // 0 = tie
+    // 1 = player wins
+    // 2 = computer wins
+
+    //First Input is Player | Second Input is Computer
+    //Rock & Rock || Paper & Paper || Scissors & Scissors - Neither Wins
+    if ((playerChoiceID === 0 && computerChoiceID === 0) || (playerChoiceID === 1 && computerChoiceID === 1) || (playerChoiceID === 2 && computerChoiceID === 2) )
+    {
+        messageID = 0;
+    }
+
+    //Rock & Paper || Paper & Scissors || Scissors & Rock- Player Loses
+    if ((playerChoiceID === 0 && computerChoiceID === 1) || (playerChoiceID === 1 && computerChoiceID === 2) || (playerChoiceID === 2 && computerChoiceID === 0) )
+    {
+        messageID = 2;
+        computerScore++;
+    }
+
+    //Rock & Scissors || Paper & Rock || Scissors & Paper - Player Wins
+    if ((playerChoiceID === 0 && computerChoiceID === 2) || (playerChoiceID === 1 && computerChoiceID === 0) || (playerChoiceID === 2 && computerChoiceID === 1))
+    {
+        messageID = 1;
+        playerScore++;
+    }
+
+    displayWinner();
+}
+
+function displayWinner(){
+    let playerMessage = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+    let computerMessage = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    // 0 = tie
+    // 1 = player wins
+    // 2 = computer wins
+    switch (messageID){
+        case 0:
+            console.log("It's a tie!");
+            break;
+        case 1:
+            console.log("You win! " + playerMessage + " beats " + computerMessage + "!");
+            break;
+        case 2:
+            console.log("You lose! " + computerMessage + " beats " + playerMessage + "!");
+    }
+}
+
+playRound();
+
